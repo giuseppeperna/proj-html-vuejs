4e32dddf4a821d088f29543e090e223e779c9beb
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-12-10 13:05:03 +0100

    Barber Shop
    Html Css completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+/**
+ @file
+
+ @author Giuseppe Perna <giuseppeperna.cg@gmail.com>
+ */
+
+ //Init Vue Object
+
+ const barberShop = new Vue({
+     el:"#barberShop",
+     data: {
+
+     },
+ });
